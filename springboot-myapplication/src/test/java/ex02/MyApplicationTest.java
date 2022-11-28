4589cfe21aa4d2239e47ea1d1c8822fdb685baf3
Son-Sumin <goldkitty007@gmail.com>
2022-11-28 09:57:47 +0900

    springboot-MyApplication #1:@SpringBootConfiguration

---
+++ springboot-myapplication/src/test/java/ex02/MyApplicationTest.java
@@ -0,0 +1,31 @@
+package ex02;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ex01.MyComponent;
+
+/**
+ * Spring Boot Test Integration 기반
+ * 
+ * (JUnit 확인, SpringBoot wk)
+ * Spring Boot Test Integration(@SpringBootTest)에서는
+ * 자동(스캔)을 위해서 설정 클래스에 @SpringBootConfiguration을 달아야한다.
+ * 
+ * @SpringBootApplication은 @SpringBootConfiguration 기능을 갖고 있다.
+ * 
+ */
+
+@SpringBootTest
+public class MyApplicationTest {
+	@Autowired
+	private MyComponent myComponent;
+	
+	@Test
+	public void myComponentNotNull() {
+		assertNotNull(myComponent);
+	}
+}
