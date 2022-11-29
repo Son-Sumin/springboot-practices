261a7c022f153c99c2325b778a64430a9fad998a
Son-Sumin <goldkitty007@gmail.com>
2022-11-29 12:52:03 +0900

    SpringBootApplication

--- springboot-myapplication/src/main/java/ex03/MyApplication.java
+++ springboot-myapplication/src/main/java/ex03/MyApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 // @SpringBootApplication은 아래 2개를 포함함
+// ex01은 @Configuration + @Bean   [Spring]
+// ex02는 @SpringBootConfiguration + @Bean   [SpringBoot]
+// ex03은 @SpringBootConfiguration  + @ComponentScan + 각각의 패키지
+
 @SpringBootConfiguration
 @ComponentScan
 public class MyApplication {
--- springboot-myapplication/src/main/java/ex04/MyApplication.java
+++ springboot-myapplication/src/main/java/ex04/MyApplication.java
@@ -13,6 +13,10 @@ import org.springframework.context.ConfigurableApplicationContext;
  *      1. MVC, AOP, Security, JPA 등을 자동 "디폴트"(***미세설정 필수-알아야한다!!***) 설정
  *      2. 발견된 Starter Dependency(Library) 기반으로 설정한다.
  *
+ * 참고) 
+ * ex01은 @Configuration + @Bean   [Spring]
+ * ex02는 @SpringBootConfiguration + @Bean   [SpringBoot]
+ * ex03은 @SpringBootConfiguration  + @ComponentScan + 각각의 패키지
  */
 @SpringBootApplication
 public class MyApplication {
